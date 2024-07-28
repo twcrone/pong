d2e5cae1e4214ea4c3d533a7cf6e486a4e823b4f
Todd W Crone <twcrone@gmail.com>
2024-07-28 16:52:14 -0400

    Initial commit

---
+++ Game.h
@@ -0,0 +1,64 @@
+// ----------------------------------------------------------------
+// From Game Programming in C++ by Sanjay Madhav
+// Copyright (C) 2017 Sanjay Madhav. All rights reserved.
+// 
+// Released under the BSD License
+// See LICENSE in root directory for full details.
+// ----------------------------------------------------------------
+
+#pragma once
+#include "SDL/SDL.h"
+
+// Vector2 struct just stores x/y coordinates
+// (for now)
+struct Vector2
+{
+	float x;
+	float y;
+};
+
+// Game class
+class Game
+{
+public:
+	Game();
+	// Initialize the game
+	bool Initialize();
+	// Runs the game loop until the game is over
+	void RunLoop();
+	// Shutdown the game
+	void Shutdown();
+private:
+	// Helper functions for the game loop
+	void ProcessInput();
+	void UpdateGame();
+	void GenerateOutput();
+
+	// Window created by SDL
+	SDL_Window* mWindow;
+	// Renderer for 2D drawing
+	SDL_Renderer* mRenderer;
+	// Number of ticks since start of game
+	Uint32 mTicksCount;
+	// Game should continue to run
+	bool mIsRunning;
+	
+	// Pong specific
+    
+    // Left Paddle
+	// Direction of paddle
+	int mLeftPaddleDir;
+	// Position of paddle
+	Vector2 mLeftPaddlePos;
+    
+    // Right Paddle
+    // Direction of paddle
+    int mRightPaddleDir;
+    // Position of paddle
+    Vector2 mRightPaddlePos;
+
+    // Position of ball
+	Vector2 mBallPos;
+	// Velocity of ball
+	Vector2 mBallVel;
+};
